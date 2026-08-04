a34d1e3df58e225848125c8eed842234bfca849a
anhtt <tuananh03122006@gmail.com>
2026-08-04 15:14:59 +0700

    innerhtml

--- index.js
+++ index.js
@@ -2,24 +2,31 @@ console.log("javascript");
 // $ PHP
 // let
 
-let name = "tuananh";
-let age = 20;
-let isStudying = true;
+// let name = "tuananh";
+// let age = 20;
+// let isStudying = true;
 
-const birtDay = 2006
+// const birtDay = 2006
 
-console.log(name);
+// console.log(name);
 
-// kiểu dữ liệu
-let greeting1 = "Hello";
-let greeting2 = "Xin chào";
+// // kiểu dữ liệu
+// let greeting1 = "Hello";
+// let greeting2 = "Xin chào";
 
-// Number
-let age = 20;
-let score = 10;
+// // Number
+// let age = 20;
+// let score = 10;
 
-// Boolean
-let isStudent = true;
-let isLoggedIn = false;
+// // Boolean
+// let isStudent = true;
+// let isLoggedIn = false;
+
+// console.log(greeting1);
+// alert("Xin chào JavaScript");
+
+const h1 = document.getElementById("title");
+h1.innerHTML="Danh sách sản phẩm";
+console.log(h1);
 
 
