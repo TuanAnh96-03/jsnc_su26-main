934c20b71c7c73f7496ae9b9e0dc9f7cec7b2239
anhtt <tuananh03122006@gmail.com>
2026-08-04 14:50:26 +0700

    1

--- index.js
+++ index.js
@@ -4,9 +4,22 @@ console.log("javascript");
 
 let name = "tuananh";
 let age = 20;
+let isStudying = true;
 
 const birtDay = 2006
 
 console.log(name);
+
+// kiểu dữ liệu
+let greeting1 = "Hello";
+let greeting2 = "Xin chào";
+
+// Number
+let age = 20;
+let score = 10;
+
+// Boolean
+let isStudent = true;
+let isLoggedIn = false;
 
 
