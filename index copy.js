746009cfdacd416d04c0303df34c58e2c11895f0
anhtt <tuananh03122006@gmail.com>
2026-08-06 14:43:29 +0700

    object lên table

---
+++ index copy.js
@@ -0,0 +1,75 @@
+console.log("javascript");
+// $ PHP
+// let
+
+// let name = "tuananh";
+// let age = 20;
+// let isStudying = true;
+
+// const birtDay = 2006
+
+// console.log(name);
+
+// // kiểu dữ liệu
+// let greeting1 = "Hello";
+// let greeting2 = "Xin chào";
+
+// // Number
+// let age = 20;
+// let score = 10;
+
+// // Boolean
+// let isStudent = true;
+// let isLoggedIn = false;
+
+// console.log(greeting1);
+// alert("Xin chào JavaScript");
+
+// const h1 = document.getElementById("title");
+// console.log(h1);
+// const title = "Laptop-Máy tính"
+// h1.innerHTML = `Danh sách ${title}`
+
+
+// let name = "Trần Tuấn Anh";
+// let age = 20;
+// let email = "tuananh03122006@gmail.com";
+// let address = "Hà Nội"
+
+// console.log(`
+//     Tên: ${name}
+//     Tuổi:${age}
+//     Email:${email}
+//     Địa chỉ:${address}
+//     `);
+
+
+// const student = {
+//   name: "Trần Tuấn Anh",
+//   age: 20,
+// };  
+
+// document.getElementById("title").innerHTML = `
+// <h2>${student.name}</h2>
+// <p>Tuổi: ${student.age}</p>
+// `;
+
+//OBJECT
+
+const student = {
+    name:"tuananh",
+    age:30,
+    student:true  
+};
+console.log(student.name);
+document.getElementById("title").innerHTML=`
+<h1>${student.name}</h1>`;
+
+
+const book ={
+    title: "Hoa vàng trên cỏ xanh",
+    author:"Nguyễn Nhật Ánh",
+    cover :"",
+    year:1999
+}
+document.getElementById("namebook").innerHTML=`<h1>${book.title}</h1>`
--- index.js
+++ index.js
@@ -1,75 +1,29 @@
-console.log("javascript");
-// $ PHP
-// let
+const tbody = document.getElementById("student")
+console.log(tbody);
 
-// let name = "tuananh";
-// let age = 20;
-// let isStudying = true;
-
-// const birtDay = 2006
-
-// console.log(name);
-
-// // kiểu dữ liệu
-// let greeting1 = "Hello";
-// let greeting2 = "Xin chào";
-
-// // Number
-// let age = 20;
-// let score = 10;
-
-// // Boolean
-// let isStudent = true;
-// let isLoggedIn = false;
-
-// console.log(greeting1);
-// alert("Xin chào JavaScript");
-
-// const h1 = document.getElementById("title");
-// console.log(h1);
-// const title = "Laptop-Máy tính"
-// h1.innerHTML = `Danh sách ${title}`
-
-
-// let name = "Trần Tuấn Anh";
-// let age = 20;
-// let email = "tuananh03122006@gmail.com";
-// let address = "Hà Nội"
-
-// console.log(`
-//     Tên: ${name}
-//     Tuổi:${age}
-//     Email:${email}
-//     Địa chỉ:${address}
-//     `);
-
-
-// const student = {
-//   name: "Trần Tuấn Anh",
-//   age: 20,
-// };  
-
-// document.getElementById("title").innerHTML = `
-// <h2>${student.name}</h2>
-// <p>Tuổi: ${student.age}</p>
-// `;
-
-//OBJECT
 
 const student = {
-    name:"tuananh",
-    age:30,
-    student:true  
-};
-console.log(student.name);
-document.getElementById("title").innerHTML=`
-<h1>${student.name}</h1>`;
+    id :10,
+    name:"tuananh"
 
-
-const book ={
-    title: "Hoa vàng trên cỏ xanh",
-    author:"Nguyễn Nhật Ánh",
-    year:1999
 }
-document.getElementById("namebook").innerHTML=`
-<h1>${book.title}</h1>`
+tbody.innerHTML = `<tr class="hover:bg-gray-50">
+              <td class="px-4 py-2 border border-gray-300">${student.id}</td>
+              <td id="namebook" class="px-4 py-2 border border-gray-300">${student.name}</td>
+              <td class="px-4 py-2 border border-gray-300">
+                <div class="flex items-center justify-center gap-2">
+                  <a
+                    href="#"
+                    class="bg-blue-500 hover:bg-blue-600 text-white px-3 py-1 rounded"
+                  >
+                    Edit
+                  </a>
+
+                  <button
+                    class="bg-red-500 hover:bg-red-600 text-white px-3 py-1 rounded"
+                  >
+                    Delete
+                  </button>
+                </div>
+              </td>
+            </tr>`
